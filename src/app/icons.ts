a9d49f8f4f4b0c5ffab49d34d7c7150b520c91f4
Daniel Karp <danielkarp@gmail.com>
2018-09-19 18:24:12 -0400

    explanatory comments and readme

--- src/app/icons.ts
+++ src/app/icons.ts
@@ -1,3 +1,5 @@
+// To test bundle analyzer, swap the import statment below with the commented out version:
+// import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
 export {
    faFacebookF,
